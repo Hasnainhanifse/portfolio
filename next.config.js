abc75f2b4ac680098d2b8f365f9ff81744beacd1
Hasnain Hanif <hasnainhanifse@gmail.com>
2024-09-02 03:30:58 +0500

    updated base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   // reactStrictMode: true,
+  basePath:isProd ? "/portfolio" : "",
   distDir: "dist",
   imagesDir: {
     unoptimized: true,
