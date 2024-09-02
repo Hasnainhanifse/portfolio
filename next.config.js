adf343cc506169282df4ca7a7ebeb8e057005614
Hasnain Hanif <hasnainhanifse@gmail.com>
2024-09-02 14:12:44 +0500

    deployment settings for vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  // reactStrictMode: true,
-  basePath:isProd ? "/portfolio" : "",
-  output: 'export',
-  distDir: "dist",
-  imagesDir: {
-    unoptimized: true,
-  }
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
